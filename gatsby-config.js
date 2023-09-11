191e43adf9ce5758c1dddc993b18946dcefa99cd
Jiaaming <2455951489@qq.com>
2023-09-11 19:25:39 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,35 @@
-/* eslint-disable no-undef */
-// eslint-disable-next-line no-undef
 module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    siteTitle: 'Jiaming\'s Running Page',
+    siteUrl: 'https://running.jiaaming.cn',
+    logo: 'https://www.helloimg.com/images/2022/07/15/ZcWGAS.jpg',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://www.jiaaming.cn',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://www.jiaaming.cn/about/',
       },
     ],
   },
   plugins: [
     'gatsby-transformer-json',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline',
-
-    {
-      resolve: 'gatsby-plugin-vercel',
-      options: {
-        // (optional) Prints metrics in the console when true
-        debug: false,
-      },
-    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: './src/static/',
+      },
+    },
+    {
+      resolve: "gatsby-plugin-vercel",
+      options: {
+        // (optional) Prints metrics in the console when true
+        debug: false,
       },
     },
     {
@@ -45,7 +41,7 @@ module.exports = {
     {
       resolve: 'gatsby-alias-imports',
       options: {
-        rootFolder: '.',
+        rootFolder: './',
       },
     },
     {
@@ -77,6 +73,32 @@ module.exports = {
         theme_color: '#e1e1e1',
         display: 'standalone',
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https?:.*\/icons\/.*\.png/,
+            handler: `CacheFirst`,
+          },
+          {
+            urlPattern:
+              /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `StaleWhileRevalidate`,
+          },
+          {
+            urlPattern: /^https?:\/\/api\.mapbox\.com\//,
+            handler: `StaleWhileRevalidate`,
+          },
+          {
+            urlPattern: /^https?:.*\/page-data\/.*\.json/,
+            handler: `StaleWhileRevalidate`,
+          },
+        ],
+        skipWaiting: true,
+        clientsClaim: true,
       },
     },
   ],
